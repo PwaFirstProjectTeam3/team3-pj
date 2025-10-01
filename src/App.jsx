e061b7ace7c1fc04c227dca9d12c4aada861797f
KMinHyun <kimdennis264@gmail.com>
2025-10-01 18:12:38 +0900

    251001 vscode 기본 파일, 폴더 생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import { Outlet } from 'react-router-dom'
 import './App.css'
+import Header from './components/common/Header'
 
 function App() {
   return (
     <>
-      
+      <Header />
+      <main>
+        <Outlet />
+      </main>
     </>
   )
 }
---
+++ src/components/common/Header.jsx
@@ -0,0 +1,13 @@
+import './Header.css';
+
+function Header() {
+
+  return(
+    <>
+    
+    
+    </>
+  )
+}
+
+export default Header;
