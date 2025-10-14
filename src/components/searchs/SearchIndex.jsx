d030c0707112b0c2aece10da52b7dae532b8c45f
KMinHyun <kimdennis264@gmail.com>
2025-10-14 17:48:28 +0900

    251014 김민현 - [구현] STATION_ROUTE_SEARCH01 작업중/configs, slice, thunk 파일 생성, store 추가

--- src/components/searchs/SearchIndex.jsx
+++ src/components/searchs/SearchIndex.jsx
@@ -1,21 +1,126 @@
+import { useDispatch, useSelector } from 'react-redux';
 import './SearchIndex.css';
+import { useEffect, useState } from 'react';
+import { clearEndResults, clearStartResults, pickEndStation, pickStartStation, setEndStation, setStartStation } from '../../store/slices/searchIndexSlice.js';
+import { searchIndexList } from '../../store/thunks/searchIndexListThunk.js';
 
 function SearchIndex() {
+  // 드랍다운 바깥 클릭 시 닫기
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (!event.target.closest('.search-box')) {
+        setShowStartDropdown(false)
+        setShowEndDropdown(false)
+      }
+    }
+
+    document.addEventListener('mousedown', handleClickOutside)
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside)
+    }
+  }, [])
+
+  const dispatch = useDispatch();
+  const {
+    startStation, endStation,
+    startSearchResults, endSearchResults,
+  } = useSelector(state => state.searchIndex);
+
+  const [showStartDropdown, setShowStartDropdown] = useState(false);
+  const [showEndDropdown, setShowEndDropdown] = useState(false);
+
+
+
+  // 출발역 검색 핸들러
+  const handleStartStationSearch = (e) => {
+    const query = e.target.value;
+    dispatch(setStartStation(query));
+
+    if (query.trim().length > 0) {
+      setShowStartDropdown(true);
+      dispatch(searchIndexList({ field: 'start', query }));
+    } else {
+      setShowStartDropdown(false);
+      dispatch(clearStartResults());
+    }
+  };
+  // 도착역 검색 핸들러
+  const handleEndStationSearch = (e) => {
+    const query = e.target.value;
+    dispatch(setEndStation(query));
+
+    if (query.trim().length > 0) {
+      setShowEndDropdown(true);
+      dispatch(searchIndexList({ field: 'end', query }));
+    } else {
+      setShowEndDropdown(false);
+      dispatch(clearEndResults());
+    }
+  };
+
+  // 출발역 목록 클릭 핸들러
+  const handleStartStationSelect = (station) => {
+    dispatch(pickStartStation({
+      name: station.station_nm,
+      id: station.station_cd,
+    }));
+    setShowStartDropdown(false);
+  };
+
+  // 도착역 목록 클릭 핸들러
+  const handleEndStationSelect = (station) => {
+    dispatch(pickEndStation({
+      name: station.station_nm,
+      id: station.station_cd,
+    }));
+    setShowEndDropdown(false);
+  };
 
   return (
     <>
       <div className="search-container">
         <div className='search-box'>
-          <input className='departure-station' type="text" placeholder='출발지' />
+          <input className='departure-station' value={startStation} onChange={handleStartStationSearch} type="text" placeholder='출발지' />
+          {showStartDropdown && (
+            <ul className="start-dropdown">
+              {startSearchResults.length > 0 && (
+                startSearchResults.map((state) => (
+                  <li
+                    key={state.STATION_CD}
+                    onClick={() => handleStartStationSelect(state)}
+                  >
+                    <span className="station-name">{state.STATION_NM}</span>
+                    <span className="station-line">{state.LINE_NUM}</span>
+                  </li>
+                ))
+              )}
+            </ul>
+          )}
           <button className='reverse-btn' type="button">
             <img src="/btn/reverse-btn.svg" alt="출발지와 도착지 교환 기능" />
           </button>
-          <input className='arrival-station' type="text" placeholder='도착지' />
+          <input className='arrival-station' value={endStation} onChange={handleEndStationSearch} type="text" placeholder='도착지' />
+          {showEndDropdown && (
+            <ul className="dropdown-end">
+              {endSearchResults.length > 0 && (
+                endSearchResults.map((state) => (
+                  <li
+                    key={state.STATION_CD}
+                    onClick={() => handleEndStationSelect(state)}
+                  >
+                    <span className="station-name">{state.STATION_NM}</span>
+                    <span className="station-line">{state.LINE_NUM}</span>
+                  </li>
+                ))
+              )}
+            </ul>
+          )}
           <div className='search-btns'>
             <button className='reset-btn' type="button">리셋</button>
             <button className='search-btn' type="button">길찾기</button>
           </div>
         </div>
+        
         <div className='search-card-container'>
           <div className="card">
             <p className='card-running-time'>16분</p>
--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import evacuationReducer from './slices/searchSlice.js';
+import searchIndexReducer from './slices/searchIndexSlice.js';
 import detailReducer from './slices/detailSlice.js';
 import subwayLinesReducer from "./slices/subwayLinesSlice";
 
 export default configureStore({
   reducer: {
+    searchIndex: searchIndexReducer,
     evacuation: evacuationReducer,
     detail: detailReducer,
     lines: subwayLinesReducer,
