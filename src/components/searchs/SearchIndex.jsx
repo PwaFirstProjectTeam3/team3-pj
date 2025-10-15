5fa5fc831355bf8e355c08bf13fde3a2341ea0e5
kimwemin <kimwemin488@gmail.com>
2025-10-15 17:06:24 +0900

    251015 작업자 김위민 [구현] STATION_ROUTE_SEARCH01 검색창 검색기능 제외 검색창 기능 작업 완료 (역 자동완성 기능 / 리셋, 리버스 버튼 등), 자동완성 기능 구현을 위한 config 파일 수정, slice / thunk 파일이 불필요해 짐에 따라 삭제 및 store 수정

--- src/components/searchs/SearchIndex.jsx
+++ src/components/searchs/SearchIndex.jsx
@@ -1,126 +1,186 @@
-import { useDispatch, useSelector } from 'react-redux';
+import { useEffect, useRef, useState } from 'react';
 import './SearchIndex.css';
-import { useEffect, useState } from 'react';
-import { clearEndResults, clearStartResults, pickEndStation, pickStartStation, setEndStation, setStartStation } from '../../store/slices/searchIndexSlice.js';
-import { searchIndexList } from '../../store/thunks/searchIndexListThunk.js';
+import { ANNOTATION_VER_ROUTE_DISPLAY } from '../../configs/line-list-configs/subwayLinesRouteConfig';
+
+// 역 이름 → 호선 맵 생성
+const stationMap = (() => {
+  const map = {};
+  Object.entries(ANNOTATION_VER_ROUTE_DISPLAY).forEach(([line, stations]) => {
+    stations.forEach(name => {
+      if (!map[name]) map[name] = [];
+      map[name].push(line);
+    });
+  });
+  return map;
+})();
+
+// 자동완성 검색에 사용할 모든 역 이름 배열
+const searchStations = Object.keys(stationMap);
 
 function SearchIndex() {
-  // 드랍다운 바깥 클릭 시 닫기
+  const departureRef = useRef(null);
+  const arrivalRef = useRef(null);
+  
+  const [departureInputValue, setDepartureInputValue] = useState(""); // 출발지 input에 입력된 역 명
+  const [arrivalInputValue, setArrivalInputValue] = useState(""); // 도착지 input에 입력된 역 명
+  const [activeField, setActiveField] = useState(null); // 출발지, 도착지 검색창 중 어떤 것이 활성화 되어있는지
+
+  // 드롭다운 열기
+  const searchOpenDropdown = (field) => {
+    setActiveField(field); // 어떤 input이 활성화 되었는지
+  };
+
+  // 드롭다운 닫기
+  function searchCloseDropdown() {
+    setActiveField(null);
+  }
+  
+  // 드롭다운 아이템 클릭 시 input에 값 넣고 드랍다운 닫기
+  function handleSearchSelectStation(station) {
+    if (activeField === "departure") setDepartureInputValue(station);
+    else if (activeField === "arrival") setArrivalInputValue(station);
+    searchCloseDropdown();
+  }
+
+  // input 클릭 시 입력값 초기화 및 드랍박스 오픈
+  // 출발지
+  function handleDepartureInputValuereset() {
+    if(departureInputValue) {
+      setDepartureInputValue('');
+
+      searchOpenDropdown("departure");
+    }
+  }
+  // 도착지
+  function handleArrivalInputValuereset() {
+    if(arrivalInputValue) {
+      setArrivalInputValue('');
+
+      searchOpenDropdown("arrival");
+    }
+  }
+
+  // 역 이름 입력 시 상태 갱신
+  function handleSearchInputChange(e, field) {
+    if (field === "departure") setDepartureInputValue(e.target.value);
+    else setArrivalInputValue(e.target.value);
+    searchOpenDropdown(field);
+  }
+  
+  // 입력값 기반 실시간 필터링
+  const searchFilteredStations = activeField === "departure" ? searchStations.filter(station => station.includes(departureInputValue)) : activeField === "arrival" ? searchStations.filter(station => station.includes(arrivalInputValue)) : [];
+
+  // 외부 클릭 시 드롭다운 닫기
   useEffect(() => {
-    const handleClickOutside = (event) => {
-      if (!event.target.closest('.search-box')) {
-        setShowStartDropdown(false)
-        setShowEndDropdown(false)
+    function handleSearchClickOutside(event) {
+      if (departureRef.current && !departureRef.current.contains(event.target) && arrivalRef.current && !arrivalRef.current.contains(event.target)) {
+        searchCloseDropdown();
       }
     }
+    document.addEventListener("mousedown", handleSearchClickOutside);
+    return () => document.removeEventListener("mousedown", handleSearchClickOutside);
+  }, []);
 
-    document.addEventListener('mousedown', handleClickOutside)
-    return () => {
-      document.removeEventListener('mousedown', handleClickOutside)
+  // 리버스 버튼
+  function reverseBtn() {
+    if(departureInputValue && arrivalInputValue) {
+      setDepartureInputValue(arrivalInputValue);
+      setArrivalInputValue(departureInputValue);
     }
-  }, [])
+  }
 
-  const dispatch = useDispatch();
-  const {
-    startStation, endStation,
-    startSearchResults, endSearchResults,
-  } = useSelector(state => state.searchIndex);
-
-  const [showStartDropdown, setShowStartDropdown] = useState(false);
-  const [showEndDropdown, setShowEndDropdown] = useState(false);
-
-
-
-  // 출발역 검색 핸들러
-  const handleStartStationSearch = (e) => {
-    const query = e.target.value;
-    dispatch(setStartStation(query));
-
-    if (query.trim().length > 0) {
-      setShowStartDropdown(true);
-      dispatch(searchIndexList({ field: 'start', query }));
-    } else {
-      setShowStartDropdown(false);
-      dispatch(clearStartResults());
+  // 리셋 버튼
+  function resetBtn() {
+    if(departureInputValue || arrivalInputValue) {
+      setDepartureInputValue("");
+      setArrivalInputValue("");
     }
-  };
-  // 도착역 검색 핸들러
-  const handleEndStationSearch = (e) => {
-    const query = e.target.value;
-    dispatch(setEndStation(query));
-
-    if (query.trim().length > 0) {
-      setShowEndDropdown(true);
-      dispatch(searchIndexList({ field: 'end', query }));
-    } else {
-      setShowEndDropdown(false);
-      dispatch(clearEndResults());
-    }
-  };
-
-  // 출발역 목록 클릭 핸들러
-  const handleStartStationSelect = (station) => {
-    dispatch(pickStartStation({
-      name: station.station_nm,
-      id: station.station_cd,
-    }));
-    setShowStartDropdown(false);
-  };
-
-  // 도착역 목록 클릭 핸들러
-  const handleEndStationSelect = (station) => {
-    dispatch(pickEndStation({
-      name: station.station_nm,
-      id: station.station_cd,
-    }));
-    setShowEndDropdown(false);
-  };
+  }
 
   return (
     <>
       <div className="search-container">
         <div className='search-box'>
-          <input className='departure-station' value={startStation} onChange={handleStartStationSearch} type="text" placeholder='출발지' />
-          {showStartDropdown && (
-            <ul className="start-dropdown">
-              {startSearchResults.length > 0 && (
-                startSearchResults.map((state) => (
-                  <li
-                    key={state.STATION_CD}
-                    onClick={() => handleStartStationSelect(state)}
-                  >
-                    <span className="station-name">{state.STATION_NM}</span>
-                    <span className="station-line">{state.LINE_NUM}</span>
-                  </li>
-                ))
-              )}
-            </ul>
-          )}
-          <button className='reverse-btn' type="button">
+          <div className='search-departure-station' ref={departureRef}>
+            <input className='departure-station'
+              value={departureInputValue}
+              onChange={e => handleSearchInputChange(e, "departure")}
+              onFocus={() => searchOpenDropdown("departure")}
+              onClick={handleDepartureInputValuereset}
+              autoComplete='off' // 브라우저 기본 자동완성 기능 끄기
+              type="text" placeholder='출발지'
+            />
+            {activeField === "departure" && searchFilteredStations.length > 0 && (
+              <ul className="search-dropdown">
+                {searchFilteredStations.map(station => (
+                  stationMap[station].length > 1
+                    ? stationMap[station].map(line => (
+                        <li
+                          key={`${station}-${line}`}
+                          onClick={() => handleSearchSelectStation(station)}
+                          onMouseDown={e => e.preventDefault()}
+                        >
+                          <span>{station}</span>
+                          <span className="dropdown-station-line">{line}</span>
+                        </li>
+                      ))
+                    : (
+                      <li
+                        key={station}
+                        onClick={() => handleSearchSelectStation(station)}
+                        onMouseDown={e => e.preventDefault()}
+                      >
+                        <span>{station}</span>
+                      </li>
+                    )
+                ))}
+              </ul>
+            )}
+          </div>
+          <button className='reverse-btn' onClick={reverseBtn} type="button">
             <img src="/btn/reverse-btn.svg" alt="출발지와 도착지 교환 기능" />
           </button>
-          <input className='arrival-station' value={endStation} onChange={handleEndStationSearch} type="text" placeholder='도착지' />
-          {showEndDropdown && (
-            <ul className="dropdown-end">
-              {endSearchResults.length > 0 && (
-                endSearchResults.map((state) => (
-                  <li
-                    key={state.STATION_CD}
-                    onClick={() => handleEndStationSelect(state)}
-                  >
-                    <span className="station-name">{state.STATION_NM}</span>
-                    <span className="station-line">{state.LINE_NUM}</span>
-                  </li>
-                ))
-              )}
+          <div className='search-arrival-station' ref={arrivalRef}>
+            <input className='arrival-station'
+              value={arrivalInputValue}
+              onChange={e => handleSearchInputChange(e, "arrival")}
+              onFocus={() => searchOpenDropdown("arrival")}
+              onClick={handleArrivalInputValuereset}
+              autoComplete='off' // 브라우저 기본 자동완성 기능 끄기
+              type="text" placeholder='도착지'
+            />
+            {activeField === "arrival" && searchFilteredStations.length > 0 && (
+              <ul className="search-dropdown">
+                {searchFilteredStations.map(station => (
+                  stationMap[station].length > 1
+                    ? stationMap[station].map(line => (
+                        <li
+                          key={`${station}-${line}`}
+                          onClick={() => handleSearchSelectStation(station)}
+                          onMouseDown={e => e.preventDefault()}
+                        >
+                          <span>{station}</span>
+                          <span className="dropdown-station-line">{line}</span>
+                        </li>
+                      ))
+                    : (
+                      <li
+                        key={station}
+                        onClick={() => handleSearchSelectStation(station)}
+                        onMouseDown={e => e.preventDefault()}
+                      >
+                        <span>{station}</span>
+                      </li>
+                    )
+                ))}
             </ul>
           )}
+          </div>
           <div className='search-btns'>
-            <button className='reset-btn' type="button">리셋</button>
+            <button className='reset-btn' onClick={resetBtn} type="button">리셋</button>
             <button className='search-btn' type="button">길찾기</button>
           </div>
         </div>
-        
         <div className='search-card-container'>
           <div className="card">
             <p className='card-running-time'>16분</p>
--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import evacuationReducer from './slices/evacuationSlice.js';
-import searchIndexReducer from './slices/searchIndexSlice.js';
 import detailReducer from './slices/detailSlice.js';
 import subwayLinesReducer from "./slices/subwayLinesSlice";
 
 export default configureStore({
   reducer: {
-    searchIndex: searchIndexReducer,
     evacuation: evacuationReducer,
     detail: detailReducer,
     lines: subwayLinesReducer,
